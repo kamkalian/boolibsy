6baa76cb7679c06385b3587a82c21cdd4eda5466
Andreas Kurm <akurm@localhost.localdomain>
2018-12-15 10:06:57 +0100

    barcode generator

--- app/routes.py
+++ app/routes.py
@@ -9,6 +9,8 @@ from flask import render_template, url_for, redirect, flash, session, request
 from app import app
 from app.forms import ScanForm
 from app.models import Reader, Book
+import hashlib, os
+from app.Bacode128Generator import Barcode128
 
 
 @app.route('/')
@@ -17,7 +19,10 @@ def index():
 	form = ScanForm()
 
 	if form.validate_on_submit():
-		return redirect(url_for('reader', reader_id=form.barcode.data))
+		barcode = form.barcode.data
+		hex_id = barcode[1:]
+		reader_id = int(hex_id, 16)
+		return redirect(url_for('reader', reader_id=reader_id))
 
 	book_count = Book.query.count()
 	reader_count = Reader.query.count()
@@ -42,8 +47,27 @@ def index():
 @app.route('/reader/<reader_id>', methods=['GET', 'POST'])
 def reader(reader_id):
 	reader = Reader.query.filter_by(id=reader_id).first()
+
 	if not reader is None:
-		return render_template('reader.html', title='Leser', reader=reader)
+
+		# hex wert bestimmen
+		hex_id = 'L' + hex(reader.id)
+
+		# barcode generieren
+		basedir = os.path.abspath(os.path.dirname(__file__))
+		barcode = Barcode128(hex_id, reader.first_name + ", " + reader.last_name)
+		barcode.save(basedir + "/static/reader_barcodes/" + hex_id)
+
+		return render_template(
+			'reader.html',
+			title=reader.first_name+', '+reader.last_name,
+			reader=reader, hex_id=hex_id
+		)
 	else:
 		flash(u'Leser wurde nicht in der Datenbank gefunden.', 'danger')
 		return redirect(url_for('index'))
+
+
+@app.route('/book_add_isbn', methods=['GET', 'POST'])
+def book_add_isbn():
+	return render_template('book_add_isbn.html', title='Buch über ISBN hinzufügen')
